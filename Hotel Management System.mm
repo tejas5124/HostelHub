0ab1af6cdb2b424bc50a1edbbe1ef10eff684432
tejas5124 <tejasgophane4142@gmail.com>
2025-06-03 09:54:46 +0530

    Add files via upload
    Initial documentation of the project

---
+++ Hotel Management System.mm
@@ -0,0 +1,154 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Hotel Management System" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1748913626022" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="0.62092125">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/standard-1.6.mm" followedTemplateLocation="template:/standard-1.6.mm" followedMapLastTime="1739534930000" show_tags="UNDER_NODES" showTagCategories="false" show_icon_for_attributes="true" show_note_icons="true"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="12" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Frontend Development" POSITION="bottom_or_right" ID="ID_843932234" CREATED="1748913537288" MODIFIED="1748914762674" HGAP_QUANTITY="98.6 pt" VSHIFT_QUANTITY="258.00001 pt">
+<edge COLOR="#ff0000"/>
+<font SIZE="14" BOLD="true"/>
+<node TEXT="ReactComponent" ID="ID_156188826" CREATED="1748913792662" MODIFIED="1748914792893">
+<font SIZE="12"/>
+</node>
+<node TEXT="DashBoard" ID="ID_767741239" CREATED="1748913810412" MODIFIED="1748914796464">
+<font SIZE="12"/>
+</node>
+<node TEXT="Booking UI" ID="ID_471125735" CREATED="1748913823782" MODIFIED="1748914800371">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="WireFrames" POSITION="bottom_or_right" ID="ID_1425995627" CREATED="1748914894867" MODIFIED="1748914926070" HGAP_QUANTITY="101.6 pt" VSHIFT_QUANTITY="22.8 pt">
+<edge COLOR="#ff0000"/>
+<font SIZE="14" BOLD="true"/>
+</node>
+<node TEXT="Backend Development" POSITION="bottom_or_right" ID="ID_1749721028" CREATED="1748913913228" MODIFIED="1748914767785" HGAP_QUANTITY="88.4 pt" VSHIFT_QUANTITY="7.2 pt">
+<edge COLOR="#7c0000"/>
+<font SIZE="14" BOLD="true"/>
+<node TEXT="REST API&apos;s" ID="ID_1529905263" CREATED="1748913976087" MODIFIED="1748914804474" HGAP_QUANTITY="21.2 pt" VSHIFT_QUANTITY="-19.2 pt">
+<font SIZE="12"/>
+</node>
+<node TEXT="MongoDB" ID="ID_1789284933" CREATED="1748914004478" MODIFIED="1748914808008" HGAP_QUANTITY="26.6 pt" VSHIFT_QUANTITY="6.6 pt">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Planning" POSITION="bottom_or_right" ID="ID_1720098964" CREATED="1748914052902" MODIFIED="1748914771598" HGAP_QUANTITY="68 pt" VSHIFT_QUANTITY="14.4 pt">
+<edge COLOR="#00007c"/>
+<font SIZE="14" BOLD="true"/>
+<node TEXT="Requirement Gathering" ID="ID_167822983" CREATED="1748914071677" MODIFIED="1748914813491" HGAP_QUANTITY="55.4 pt" VSHIFT_QUANTITY="-45 pt">
+<font SIZE="12"/>
+</node>
+<node TEXT="ER Diagram" ID="ID_1683232024" CREATED="1748914094945" MODIFIED="1748914818146" HGAP_QUANTITY="62 pt">
+<font SIZE="12"/>
+</node>
+<node TEXT="MERN(as a Framework)" ID="ID_431130596" CREATED="1748914129331" MODIFIED="1748914823214" HGAP_QUANTITY="59 pt" VSHIFT_QUANTITY="24.6 pt">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="StakeHolders" POSITION="bottom_or_right" ID="ID_1371254085" CREATED="1748914271228" MODIFIED="1748914777484" HGAP_QUANTITY="35 pt" VSHIFT_QUANTITY="-30.6 pt">
+<edge COLOR="#007c00"/>
+<font SIZE="14" BOLD="true"/>
+<node TEXT="Students" ID="ID_239977471" CREATED="1748914300160" MODIFIED="1748914827352" HGAP_QUANTITY="93.8 pt" VSHIFT_QUANTITY="-31.8 pt">
+<font SIZE="12"/>
+</node>
+<node TEXT="Hostel Owner" ID="ID_1168437609" CREATED="1748914312090" MODIFIED="1748914831121" HGAP_QUANTITY="83 pt" VSHIFT_QUANTITY="-23.4 pt">
+<font SIZE="12"/>
+</node>
+<node TEXT="Development Team" ID="ID_758352058" CREATED="1748914328694" MODIFIED="1748914836339" HGAP_QUANTITY="67.4 pt" VSHIFT_QUANTITY="18 pt">
+<font SIZE="12"/>
+</node>
+<node TEXT="College Admins" ID="ID_1776121733" CREATED="1748914346973" MODIFIED="1748914840778" HGAP_QUANTITY="75.8 pt" VSHIFT_QUANTITY="46.8 pt">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Intergration &amp; Testing" POSITION="bottom_or_right" ID="ID_51324755" CREATED="1748914404300" MODIFIED="1748914782101" HGAP_QUANTITY="50 pt" VSHIFT_QUANTITY="-77.4 pt">
+<edge COLOR="#007c7c"/>
+<font SIZE="14" BOLD="true"/>
+<node TEXT="Connect Frontend &amp; Backend" ID="ID_339689825" CREATED="1748914426122" MODIFIED="1748914845182" HGAP_QUANTITY="25.4 pt" VSHIFT_QUANTITY="-20.4 pt">
+<font SIZE="12"/>
+</node>
+<node TEXT="Testing &amp; Bug Fixing" ID="ID_1611757606" CREATED="1748914469814" MODIFIED="1748914849647" HGAP_QUANTITY="27.8 pt" VSHIFT_QUANTITY="26.4 pt">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Final Touches" POSITION="bottom_or_right" ID="ID_1474895191" CREATED="1748914491716" MODIFIED="1748914787313" HGAP_QUANTITY="59 pt" VSHIFT_QUANTITY="-283.80001 pt">
+<edge COLOR="#7c7c00"/>
+<font SIZE="14" BOLD="true"/>
+<node TEXT="Documentation" ID="ID_258462706" CREATED="1748914503511" MODIFIED="1748914855065" VSHIFT_QUANTITY="-16.8 pt">
+<font SIZE="12"/>
+</node>
+<node TEXT="Submission" ID="ID_556646404" CREATED="1748914516273" MODIFIED="1748914859939" HGAP_QUANTITY="20.6 pt" VSHIFT_QUANTITY="19.8 pt">
+<font SIZE="12"/>
+</node>
+</node>
+</node>
+</map>
